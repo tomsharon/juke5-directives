bb3c78a7f1b274a00fb6405dd9ceb903ca12ebde
Matthew Kim <matthewkim93@gmail.com>
2015-12-08 18:16:14 -0500

    added comment

--- browser/js/directives/doubleClick.js
+++ browser/js/directives/doubleClick.js
@@ -2,6 +2,8 @@ app.directive('doubleClick', function() {
 	return {
 		restrict: 'A',
 		scope: {
+			//& means that double click will be assigned to a function
+			//The “&” operator allows you to invoke or evaluate an expression on the parent scope of whatever the directive is inside of.
 			doubleClick: '&'
 		},
 		link: function(scope, element, attrs) {
